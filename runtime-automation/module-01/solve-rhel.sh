ac3cb24021ba77a4f56aacb644b3292d87c7e51d
Alberto Gonzalez <josegonz@redhat.com>
2025-04-16 00:25:39 +0200

    use root

--- runtime-automation/module-01/solve-rhel.sh
+++ runtime-automation/module-01/solve-rhel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ $(grep -c "makes it seem more manageable." /home/rhel/quote.txt 2>/dev/null) -eq 0 ]; then 
-    echo "makes it seem more manageable." >> /home/rhel/quote.txt
+if [ $(grep -c "makes it seem more manageable." /root/quote.txt 2>/dev/null) -eq 0 ]; then 
+    echo "makes it seem more manageable." >> /root/quote.txt
     exit 0
 fi
--- runtime-automation/module-01/validation-rhel.sh
+++ runtime-automation/module-01/validation-rhel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ $(grep -c "makes it seem more manageable." /home/rhel/quote.txt 2>/dev/null) -eq 0 ]; then 
+if [ $(grep -c "makes it seem more manageable." /root/quote.txt 2>/dev/null) -eq 0 ]; then 
     echo "quote.txt has not the correct line"; 
     exit -1
 fi
